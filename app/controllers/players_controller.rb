4e49f6f265aafc426b6391baed1552c7f81794ec
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-08-30 17:15:58 -0400

    fixed player pages

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -24,4 +24,21 @@ class PlayersController < ApplicationController
     @players = Player.all.to_a.sort_by(&:latest_rank)
   end
 
+  def show
+    @player = Player.find(params[:id])
+    @similar_players = []
+      five_lower_players = Month.last.dlf_ranks.where("rank < #{@player.latest_rank} AND rank >= #{@player.latest_rank - 5}")
+      five_higher_players = Month.last.dlf_ranks.where("rank > #{@player.latest_rank} AND rank <= #{@player.latest_rank + 5}")
+
+      five_lower_players.each do |p|
+        @similar_players << p.player
+      end
+
+      @similar_players << @player
+
+      five_higher_players.each do |p|
+        @similar_players << p.player
+      end
+  end
+
 end
